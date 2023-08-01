f2acb01418cc51294362991abf5bdc8eeb41bdfb
light <55218925+light-planck@users.noreply.github.com>
2023-08-01 09:52:21 +0900

    refactor: 暦週の日を計算するロジックを修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -27,22 +27,8 @@ Week = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
 puts Week.join(" ")
 
 # 該当月の1日にマッチする曜日を見つける
-w = 0
-not_found = true
-while not_found
-  if (Date.new(year, month, 1).wday == w)
-    print " 1 "
-    not_found = false
-  else
-    print "   "
-  end
-  
-  # 曜日を計算
-  w += 1
-
-  # 土曜なら改行
-  puts "" if (w == 7)
-end
+print "   " * today.cwday
+print " 1 "
 
 # 2日から月末まで表示
 last = Date.new(year, month, -1)
