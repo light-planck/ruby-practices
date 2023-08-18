7e60acaf6b3638149d776e2baf17886e57d3d7fd
light <55218925+light-planck@users.noreply.github.com>
2023-08-18 09:37:10 +0900

    refactor:

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -9,12 +9,11 @@ scores = score.split(',')
 
 # 数字に変換
 shots = []
-FRAME = 10 # フレーム数
+FRAMES = 10 # フレーム数
 scores.each do |s|
   if s == 'X'
     shots << 10
-    # 投げなかった
-    shots << -1 if shots.size < 2 * FRAME - 1
+    shots << 0 if shots.size < 2 * FRAMES - 1
   else
     shots << s.to_i
   end
@@ -23,31 +22,37 @@ end
 # フレーム毎に分割
 frames = []
 
-(0...FRAME * 2).step(2).each do |i|
-  frames << if i < (FRAME - 1) * 2
-              [shots[i], shots[i + 1]]
-            else
-              shots[(i..)]
-            end
+shots.each_slice(2) do |s|
+  frames << s
+end
+
+if frames.size > FRAMES
+  frames[-2].concat(frames[-1])
+  frames.slice!(FRAMES)
 end
 
 point = 0
 
 frames.each_with_index do |frame, i|
-  frame.each do |p|
-    point += p if p >= 0
-  end
+  point += frame.sum
 
-  break if i == frames.size - 1
+  break if i + 1 >= FRAMES
+
+  next_frame = frames[i + 1]
 
   # ストライク
   if frame.first == 10
-    point += frames[i + 1].first
-    point += frames[i + 1][1] >= 0 ? frames[i + 1][1] : frames[i + 2].first
+    point += next_frame.first
+
+    point += if next_frame.first < 10 || i + 2 >= FRAMES
+               next_frame[1]
+             else
+               frames[i + 2].first
+             end
 
   # スペア
   elsif frame.sum == 10
-    point += frames[i + 1].first
+    point += next_frame.first
   end
 end
 
