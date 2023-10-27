722ad37e0115e908ee60dd2c6da3476ee5ce6a48
light-planck <seseifunc16@gmail.com>
2023-10-27 19:17:32 +0900

    feat: 標準入力に対応

--- wc.rb
+++ wc.rb
@@ -4,42 +4,35 @@
 require 'optparse'
 
 def main
-  std_input if ARGV.empty?
+  options = parse_options
 
-  opt = OptionParser.new
-  options = ARGV.getopts('l', 'w', 'c')
-  opt.parse!(ARGV)
+  if ARGV.empty?
+    file = readlines
+    file_info = get_file_info(file.join)
+    single_output(file_info)
+    exit
+  end
 
   file_names = ARGV
-
-  total = total = { lines: 0, words: 0, bytes: 0, name: 'total' }
   max_filename_length = calculate_max_filename_length(file_names)
 
   file_names.each do |file_name|
     file = File.read(file_name)
-    lines = file.count("\n")
-    words = file.split(/\s+/).size
-    bytes = File.size(file_name)
-
-    output({ lines:, words:, bytes:, name: file_name }, max_filename_length, options)
-
-    total[:lines] += lines
-    total[:words] += words
-    total[:bytes] += bytes
+    file_info = get_file_info(file, file_name)
+    output(file_info, max_filename_length, options)
   end
 
-  output(total, max_filename_length, options) if file_names.size > 1
+  return if file_names.size <= 1
+
+  total_info = calculate_total_file_info(file_names)
+  output(total_info, max_filename_length, options) if file_names.size > 1
 end
 
-def std_input
-  file = readlines.join
-
-  lines = file.count("\n")
-  words = file.split(/\s+/).size
-  bytes = file.bytesize
-
-  puts "#{lines.to_s.rjust(8)} #{words.to_s.rjust(7)} #{bytes.to_s.rjust(7)}"
-  exit
+def parse_options
+  opt = OptionParser.new
+  options = ARGV.getopts('l', 'w', 'c')
+  opt.parse!(ARGV)
+  options
 end
 
 def calculate_max_filename_length(file_names)
@@ -55,14 +48,45 @@ def calculate_max_filename_length(file_names)
   max_filename_length
 end
 
-def output(file, max_filename_length, options)
+def get_file_info(file, file_name = '')
+  lines = file.count("\n")
+  words = file.split(/\s+/).size
+  bytes = file.bytesize
+  { lines:, words:, bytes:, name: file_name }
+end
+
+def calculate_total_file_info(file_names)
+  total_info = { lines: 0, words: 0, bytes: 0, name: 'total' }
+  file_names.each do |file_name|
+    file = File.read(file_name)
+    file_info = get_file_info(file, file_name)
+    total_info[:lines] += file_info[:lines]
+    total_info[:words] += file_info[:words]
+    total_info[:bytes] += file_info[:bytes]
+  end
+  total_info
+end
+
+def single_output(file)
+  width = 10
+
+  length = { lines: 0, words: 0, bytes: 0 }
+  file.each do |key, value|
+    length[key] = value.to_s.length
+  end
+
+  print "#{file[:lines].to_s.rjust(width - length[:lines], ' ')} "
+  print "#{file[:words].to_s.rjust(width - length[:words], ' ')} "
+  puts file[:bytes].to_s.rjust(width - length[:bytes], ' ')
+end
+
+def output(file, max_filename_length = {}, options = {})
   base_padding = 4
 
   output_string = format("%#{max_filename_length[:lines] + base_padding}s", file[:lines])
   output_string += format("%#{max_filename_length[:words] + base_padding}s", file[:words])
   output_string += format("%#{max_filename_length[:bytes] + base_padding}s", file[:bytes])
   output_string += " #{file[:name]}"
-
   puts output_string
 end
 
