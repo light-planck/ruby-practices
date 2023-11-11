973962259e743203dc69eafcaaf3f91965539946
light-planck <seseifunc16@gmail.com>
2023-11-11 20:16:03 +0900

    refactor: ファイルの文字数などの情報とファイル名を分離

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -9,28 +9,28 @@ def main
 
   # 標準入力がある場合
   if file_names.empty?
-    file_info = get_file_info($stdin.read)
-    output(file_info, { lines: 0, words: 0, bytes: 0 }, options)
+    file_stats = get_file_stats($stdin.read)
+    output(file_stats, '', { lines: 0, words: 0, bytes: 0 }, options)
     exit
   end
 
   max_filename_length = calculate_max_filenames_length(file_names)
-  total_info = { lines: 0, words: 0, bytes: 0, name: 'total' }
+  total_stats = { lines: 0, words: 0, bytes: 0 }
 
   file_names.each do |file_name|
     file = File.read(file_name)
-    file_info = get_file_info(file, file_name)
+    file_stats = get_file_stats(file)
 
-    file_info.reject { |key| key == :name }.each do |key, value|
-      total_info[key] += value
+    file_stats.each do |key, value|
+      total_stats[key] += value
     end
 
-    output(file_info, max_filename_length, options)
+    output(file_stats, file_name, max_filename_length, options)
   end
 
   return if file_names.size <= 1
 
-  output(total_info, max_filename_length, options)
+  output(total_stats, 'total', max_filename_length, options)
 end
 
 def parse_options
@@ -46,31 +46,35 @@ def calculate_max_filenames_length(file_names)
 
   file_names.each do |file_name|
     file = File.read(file_name)
-    file_info = get_file_info(file, file_name)
+    file_stats = get_file_stats(file)
 
-    file_info.each do |key, value|
+    file_stats.each do |key, value|
       max_filenames_length[key] = [max_filenames_length[key], value.to_s.length].max
     end
+    max_filenames_length[:name] = [max_filenames_length[:name], file_name.length].max
   end
 
   max_filenames_length
 end
 
-def get_file_info(file, file_name = '')
+def get_file_stats(file)
   lines = file.count("\n")
   words = file.split(/\s+/).size
   bytes = file.bytesize
-  { lines:, words:, bytes:, name: file_name }
+  { lines:, words:, bytes: }
 end
 
-def output(file, max_filename_length = {}, options = {})
+def output(file_stats, file_name, max_filename_length = {}, options = {})
   base_padding = 10
+  option_keys = { lines: 'l', words: 'w', bytes: 'c' }.freeze
 
   output_string = ''
-  output_string += format("%#{base_padding - max_filename_length[:lines]}s", file[:lines]) if options['l']
-  output_string += format("%#{base_padding - max_filename_length[:words]}s", file[:words]) if options['w']
-  output_string += format("%#{base_padding - max_filename_length[:bytes]}s", file[:bytes]) if options['c']
-  output_string += " #{file[:name]}" if file[:name] != ''
+  file_stats.each do |key, value|
+    output_string += format("%#{base_padding - max_filename_length[key]}s", value) if options[option_keys[key]]
+  end
+
+  output_string += " #{file_name}" if file_name != ''
+
   puts output_string
 end
 
