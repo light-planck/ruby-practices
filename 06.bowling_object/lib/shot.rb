87ca7e457555fc18f86ead6a37aa4b3cd772362c
light-planck <seseifunc16@gmail.com>
2023-12-22 17:34:22 +0900

    refactor: scoreをメソッドにする

--- 06.bowling_object/lib/shot.rb
+++ 06.bowling_object/lib/shot.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class Shot
-  attr_reader :score
+  attr_reader :mark
 
   def initialize(mark)
     raise if mark != 'X' && !(0..9).cover?(mark.to_i)
 
-    @score = mark == 'X' ? 10 : mark.to_i
+    @mark = mark
+  end
+
+  def score
+    mark == 'X' ? 10 : mark.to_i
   end
 end
