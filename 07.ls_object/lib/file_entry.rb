6975df72c331842def28487797f6182dce9b48f8
light-planck <seseifunc16@gmail.com>
2024-01-10 20:15:32 +0900

    feat: file entryオブジェクトの作成

---
+++ 07.ls_object/lib/file_entry.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'etc'
+require_relative 'permission'
+
+class FileEntry
+  def initialize(filename)
+    fs = File.lstat("./#{filename}")
+
+    @blocks = fs.blocks
+    @permission = Permission.permission(fs.mode)
+    @nlink = fs.nlink.to_s
+    @owner = Etc.getpwuid(fs.uid).name
+    @group = Etc.getgrgid(fs.gid).name
+    @size = fs.size.to_s
+    @month = fs.mtime.month.to_s
+    @day = fs.mtime.day.to_s
+    @time = "#{format('%02d', fs.mtime.hour)}:#{format('%02d', fs.mtime.sec)}"
+    @filename = filename
+    @file = File.symlink?(filename) ? "#{filename} -> #{File.readlink(filename)}" : filename
+  end
+end
