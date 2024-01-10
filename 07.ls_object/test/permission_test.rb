7049035bcadf55c7f6d17f9c2b7ee3232b4e89c7
light-planck <seseifunc16@gmail.com>
2024-01-10 19:31:50 +0900

    feat: permissionオブジェクトの作成

---
+++ 07.ls_object/test/permission_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../lib/permission'
+
+class PermissionTest < Minitest::Test
+  def test_permission
+    assert_equal 'drwxr-xr-x@', Permission.permission('16877'.to_i)
+    assert_equal '-rw-r--r--@', Permission.permission('33188'.to_i)
+    assert_equal '-rwxr-xr-x@', Permission.permission('33261'.to_i)
+  end
+end
