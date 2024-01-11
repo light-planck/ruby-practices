21f5823de3d48228dbb2c5904920b426631d49d6
light-planck <seseifunc16@gmail.com>
2024-01-11 18:55:20 +0900

    feat: cliのハンドラを作成

---
+++ 07.ls_object/lib/cli_handler.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require_relative 'directory'
+require_relative 'directory_utilities'
+
+class CLIHandler
+  def initialize
+    @options = parse_options
+    @directory = Directory.new(@options)
+  end
+
+  def execute
+    formatted_entries = DirectoryUtilities.to_short_format(@directory)
+    DirectoryUtilities.print_short_format(formatted_entries)
+  end
+
+  private
+
+  def parse_options
+    opt = OptionParser.new
+    options = {}
+    opt.on('-a') { |v| options[:a] = v }
+    opt.on('-r') { |v| options[:r] = v }
+    opt.on('-l') { |v| options[:l] = v }
+    opt.parse(ARGV)
+    options
+  end
+end
