d7332218696249af61d3a31cfd8398ee8cea00e4
light-planck <seseifunc16@gmail.com>
2024-01-12 11:16:08 +0900

    fix: testの修正

--- 07.ls_object/test/permission_test.rb
+++ 07.ls_object/test/permission_test.rb
@@ -5,8 +5,8 @@ require_relative '../lib/permission'
 
 class PermissionTest < Minitest::Test
   def test_permission
-    assert_equal 'drwxr-xr-x@', Permission.permission('16877'.to_i)
-    assert_equal '-rw-r--r--@', Permission.permission('33188'.to_i)
-    assert_equal '-rwxr-xr-x@', Permission.permission('33261'.to_i)
+    assert_equal 'drwxr-xr-x', Permission.permission('16877'.to_i)
+    assert_equal '-rw-r--r--', Permission.permission('33188'.to_i)
+    assert_equal '-rwxr-xr-x', Permission.permission('33261'.to_i)
   end
 end
