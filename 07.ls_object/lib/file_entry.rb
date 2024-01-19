926f3bdbe382c7462f2d2f6469922e458aaaff6b
light-planck <seseifunc16@gmail.com>
2024-01-19 12:14:35 +0900

    refactor: file_entryの各情報はメソッドにして常に最新の情報を取得するようにする

--- 07.ls_object/lib/file_entry.rb
+++ 07.ls_object/lib/file_entry.rb
@@ -4,21 +4,46 @@ require 'etc'
 require_relative 'permission'
 
 class FileEntry
-  attr_reader :blocks, :permission, :nlink, :owner, :group, :size, :month, :day, :time, :filename, :file
+  attr_reader :filename
 
   def initialize(filename)
-    fs = File.lstat("./#{filename}")
-
-    @blocks = fs.blocks
-    @permission = Permission.permission(fs.mode)
-    @nlink = fs.nlink.to_s
-    @owner = Etc.getpwuid(fs.uid).name
-    @group = Etc.getgrgid(fs.gid).name
-    @size = fs.size.to_s
-    @month = fs.mtime.month.to_s
-    @day = fs.mtime.day.to_s
-    @time = "#{format('%02d', fs.mtime.hour)}:#{format('%02d', fs.mtime.sec)}"
+    @fs = File.lstat("./#{filename}")
     @filename = filename
-    @file = File.symlink?(filename) ? "#{filename} -> #{File.readlink(filename)}" : filename
+  end
+
+  def blocks
+    @fs.blocks
+  end
+
+  def permission
+    Permission.permission(@fs.mode)
+  end
+
+  def nlink
+    @fs.nlink.to_s
+  end
+
+  def owner
+    Etc.getpwuid(@fs.uid).name
+  end
+
+  def group
+    Etc.getgrgid(@fs.gid).name
+  end
+
+  def size
+    @fs.size.to_s
+  end
+
+  def day
+    @fs.mtime.day.to_s
+  end
+
+  def time
+    "#{format('%02d', @fs.mtime.hour)}:#{format('%02d', fs.mtime.sec)}"
+  end
+
+  def file
+    File.symlink?(@filename) ? "#{@filename} -> #{File.readlink(@filename)}" : @filename
   end
 end
